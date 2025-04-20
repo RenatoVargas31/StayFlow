8c79ea3fd2d010a86dc008be5f18c02e9dcba096
SheilaJB <sajbalboa@hotmail.com>
2025-04-20 03:26:19 -0500

    cambio de color

--- app/src/main/java/com/codebnb/stayflow/driver/reserva/DetallesReservaActivity.java
+++ app/src/main/java/com/codebnb/stayflow/driver/reserva/DetallesReservaActivity.java
@@ -1,4 +1,4 @@
-package com.codebnb.stayflow;
+package com.codebnb.stayflow.driver.reserva;
 
 import android.os.Bundle;
 
@@ -7,18 +7,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-/*
-public class DriverReservaActivity extends AppCompatActivity {
+
+import com.codebnb.stayflow.R;
+
+public class DetallesReservaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_driver_reserva);
+        setContentView(R.layout.activity_detalles_reserva);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-} */
+}
